70f8dc53b96f932046de3e3690a96cdafb705eaa
Arpit Thakur <arpitthakut722@gmail.com>
2020-06-07 13:40:26 +0530

    added numquote

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const quote = document.getElementById ('quote')
-const author = document.getElementById('author')
-const button = document.getElementById ('next')
+const quote = document.getElementById ('quote');
+const author = document.getElementById('author');
+const button = document.getElementById ('next');
+
+let numQuote = 0;
+let numQuote1 = [0];
+button.addEventListener('click', () => {
+  do {
+    numQuote = Math.floor(Math.random() * quotes.length);
+  } while (numQuote1[numQuote1.length - 1] === numQuote);
